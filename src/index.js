ded432f1671b75dc0267f6448ea2bed11a13ee8a
17revking <871602699@qq.com>
2023-03-01 14:32:12 +0800

    react爱彼迎项目

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from "react-router-dom"
-import { Provider } from "react-redux"
-import { ThemeProvider } from 'styled-components'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 
-import App from '@/App'
-import "normalize.css"
-import "antd/dist/antd.less"
-import "./assets/css/index.less"
-import store from './store'
-import theme from './assets/theme'
+import App from "@/App";
+import "normalize.css";
+import "antd/dist/antd.less";
+import "./assets/css/index.less";
+import store from "./store";
+import theme from "./assets/theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
       <HashRouter>
-        <App/>
+        <App />
       </HashRouter>
     </ThemeProvider>
   </Provider>
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,28 @@
-import Demo from "@/views/demo"
-import React from "react"
-import { Navigate } from "react-router-dom"
+import React from "react";
+import { Navigate } from "react-router-dom";
 
-const Home = React.lazy(() => import("@/views/home"))
+const Home = React.lazy(() => import("@/views/home"));
 // import Home from "@/views/home"
-const Entire = React.lazy(() => import("@/views/entire"))
-const Detail = React.lazy(() => import("@/views/detail"))
+const Entire = React.lazy(() => import("@/views/entire"));
+const Detail = React.lazy(() => import("@/views/detail"));
 
 const routes = [
   {
     path: "/",
-    element: <Navigate to="/home"/>
+    element: <Navigate to="/home" />,
   },
   {
     path: "/home",
-    element: <Home/>
+    element: <Home />,
   },
   {
     path: "/entire",
-    element: <Entire/>
+    element: <Entire />,
   },
   {
     path: "/detail",
-    element: <Detail/>
+    element: <Detail />,
   },
-  {
-    path: "/demo",
-    element: <Demo/>
-  }
-]
+];
 
-export default routes
+export default routes;
